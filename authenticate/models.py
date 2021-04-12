65d0c77bd180bda4e7d63fce454c02a3315a9f93
tushar2407 <tushar.mohan2001@gmail.com>
2021-04-12 19:28:34 +0530

    social media handles

--- authenticate/models.py
+++ authenticate/models.py
@@ -28,6 +28,12 @@ class Profile(models.Model):
     group = models.CharField(max_length = 256)
     skills = models.ManyToManyField(Skill, related_name='people')
     
+    # social media handles
+    facebook = models.URLField(null = True)
+    instagram = models.URLField(null = True)
+    twitter = models.URLField(null = True)
+    github = models.URLField(null = True)
+
     def __str__(self):
         return f'{self.user.username} {str(self.designation)}'
 
