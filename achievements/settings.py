aaae0561b6610ae2fbaf5afef068895eac2d1ad3
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-12-28 18:36:07 +0530

    bug in settings

--- achievements/settings.py
+++ achievements/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from dotenv import load_env
-load_env()
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
